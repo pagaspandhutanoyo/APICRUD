3761cc770f9772e637042fe89fbe96d5faf31206
Pagas Pandhu Tanoyo <72847139+pagaspandhutanoyo@users.noreply.github.com>
2022-06-13 08:54:57 +0700

    update

--- config/config.js
+++ config/config.js
@@ -31,3 +31,19 @@ module.exports = {
     "dialect": DB_DIALECT
   }
 }
+const createTcpPool = async config => {
+  // Extract host and port from socket address
+  const dbSocketAddr = process.env.DB_HOSTNAME.split(':');
+
+  // Establish a connection to the database
+  return mysql.createPool({
+    user: process.env.DB_USERNAME, // e.g. 'my-db-user'
+    password: process.env.DB_PASSWORD, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    host: dbSocketAddr[0], // e.g. '127.0.0.1'
+    port: dbSocketAddr[1], // e.g. '3306'
+    // ... Specify additional properties here.
+    ...config,
+  });
+};
+
